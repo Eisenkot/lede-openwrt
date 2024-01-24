4a7fa28f58ef05004676f8eda64aaed7f016465d
Eisenkot <157346250+Eisenkot@users.noreply.github.com>
2024-01-24 21:01:56 +0800

    Add files via upload

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,6 +18,9 @@
 # echo '### Updates Theme Argon ###'
 # package/lean/luci-theme-argon /package/feeds/luci/luci-theme-argon
 # rm -rf package/feeds/luci/luci-theme-argon
+curl -o ./feeds/packages/lang/rust/rust-values.mk https://raw.githubusercontent.com/Jason6111/packages/patch-1/lang/rust/rust-values.mk
+curl -o ./feeds/packages/lang/rust/Makefile https://raw.githubusercontent.com/Jason6111/OpenWrt_Personal/main/other/rust/Makefile
+curl -o ./feeds/packages/lang/rust/patches/0001-Update-xz2-and-use-it-static.patch https://raw.githubusercontent.com/Jason6111/OpenWrt_Personal/main/other/rust/0001-Update-xz2-and-use-it-static.patch
 git clone -b luci-smartdns-dev https://github.com/xiaorouji/openwrt-passwall.git package/passwall
 # git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
 # echo '###  ###'
