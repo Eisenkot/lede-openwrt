5fde3f970f9dd0e62e6f4e17e7927bca9dfc4493
Eisenkot <157346250+Eisenkot@users.noreply.github.com>
2024-01-26 23:40:23 +0800

    Add files via upload

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,6 +18,6 @@
 # echo '### Updates Theme Argon ###'
 # package/lean/luci-theme-argon /package/feeds/luci/luci-theme-argon
 # rm -rf package/feeds/luci/luci-theme-argon
-git clone -b luci-smartdns-dev https://github.com/xiaorouji/openwrt-passwall.git package/passwall
+# git clone -b luci-smartdns-dev https://github.com/xiaorouji/openwrt-passwall.git package/passwall
 # git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
 # echo '###  ###'
