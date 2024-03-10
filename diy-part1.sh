59efbc7a268fa7551a923329f576db97fe39cf32
Eisenkot <157346250+Eisenkot@users.noreply.github.com>
2024-03-11 00:57:07 +0800

    Add files via upload

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,7 +11,7 @@
 #
 
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 # sed -i 's/CGO_ENABLED=0/CGO_ENABLED=1/g' feeds/packages/net/mosdns/Makefile
 # Add a feed source
 # echo 'src-git helloworld https://github.com/fw876/helloworld.git;main' >>feeds.conf.default
